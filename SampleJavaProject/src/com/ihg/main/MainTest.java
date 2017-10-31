1f9e8ac107fc0570c5c7b3f23cdadbd7bec2cb6f
shyam1981 <33229505+shyam1981@users.noreply.github.com>
2017-10-31 20:02:14 +0530

    Update MainTest.java
    Added sys out.

--- SampleJavaProject/src/com/ihg/main/MainTest.java
+++ SampleJavaProject/src/com/ihg/main/MainTest.java
@@ -7,6 +7,7 @@ public class MainTest {
 		System.out.println("-----------1-----------");
 		System.out.println("-----------2-----------");
 		System.out.println("-----------3-----------");
+		System.out.println("-----------4-----------");
 
 	}
 
